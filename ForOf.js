52c2b7943fc8a770e049da21758db31af733b3bf
Truong Long <truongtieulong00@gmail.com>
2020-09-22 14:20:21 +0000

    For of, lặp các key có trong mảng(array))

---
+++ ForOf.js
@@ -0,0 +1,10 @@
+/**
+ *  for of lặp các key có trong mảng(array)
+ */
+var students=[
+  { name: 'Long', age:18},
+  { name: 'Mi', age: 17}
+]
+for(var student of students){
+  console.log(student)
+}
